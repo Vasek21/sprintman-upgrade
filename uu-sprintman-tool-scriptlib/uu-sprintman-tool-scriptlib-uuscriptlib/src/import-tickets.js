2f17f15236c9faa97d8483ff07c60cbd43eef18d
Pavel Burianek <p.buri@seznam.cz>
2024-03-28 10:08:12 +0100

    Added console and progress

--- uu-sprintman-tool-scriptlib/uu-sprintman-tool-scriptlib-uuscriptlib/src/import-tickets.js
+++ uu-sprintman-tool-scriptlib/uu-sprintman-tool-scriptlib-uuscriptlib/src/import-tickets.js
@@ -3,6 +3,7 @@ const { Validator } = require("uu_appg01_server").Validation;
 const { ValidationHelper } = require("uu_appg01_server").AppServer;
 const { UseCaseError } = require("uu_appg01_server").AppServer;
 const { UriBuilder } = require("uu_appg01_server").Uri;
+const crypto = require("crypto");
 
 const CHAR_MAP = {
   128: "",
@@ -30,22 +31,35 @@ const CHAR_MAP = {
   253: "ý",
 };
 
-SPRINTMAN_OIDC_URI = "https://uuapp-dev.plus4u.net/uu-oidc-maing02/eca71064ecce44b0a25ce940eb8f053d/oidc";
-HOLLY_CREDENTIALS = {
+const SPRINTMAN_OIDC_URI = "https://uuapp-dev.plus4u.net/uu-oidc-maing02/eca71064ecce44b0a25ce940eb8f053d/oidc";
+const HOLLY_CREDENTIALS = {
   username: "Holly_Hudson",
   password: "Cq%iaz#9w-#z7C8yZlZoC$DsebIK2JGWyDK8lg8J7vLFSKXtYFt5tn3eI3WthZtI",
 };
 
-PRIORITY_MAP = {
+const PRIORITY_MAP = {
   "Must have!": "must",
   "Should have.": "should",
   "Could have.": "could",
+};
+
+const PROGRESS_USE_CASE_MAP = {
+  get: "progress/get",
+  create: "progress/create",
+  start: "progress/start",
+  proceed: "progress/proceed",
+  end: "progress/end",
+  releaseLock: "progress/releaseLock",
+  setUp: "progress/setUp",
+  setState: "progress/setState",
+  delete: "progress/delete",
 };
 
 const dtoInSchema = `
   const createReportBaselineDtoInType = shape({
     attachmentUri: uri().isRequired(),
     sprintmanBaseUri: uri().isRequired(),
+    consoleBaseUri: uri().isRequired(),
     sprintId: id().isRequired(),
   });
 `;
@@ -71,6 +85,129 @@ const Errors = {
 const Warnings = {
   unsupportedKeys: `${Errors.ERROR_PREFIX}unsupportedKeys`,
 };
+
+const { dtoIn, console: uuConsole, session } = scriptContext;
+let uuProgress;
+
+//@@viewOn:UuProgress
+// FIXME UuProgress will be a server/script library similar to UuConsole
+class UuProgress {
+  constructor({ code, name, lockSecret, consoleBaseUri, session }) {
+    this.code = code;
+    this.name = name;
+    this.lockSecret = lockSecret || crypto.randomBytes(32).toString("hex");
+    this.progressAppClient = new AppClient({
+      baseUri: consoleBaseUri,
+      session,
+    });
+  }
+}
+
+UuProgress.prototype.getOrCreate = async function (customDtoIn) {
+  let progress;
+
+  // try to get progress with given code
+  try {
+    progress = await this.progressAppClient.get(PROGRESS_USE_CASE_MAP.get, {
+      code: this.code,
+    });
+  } catch (e) {
+    uuConsole.warning(objectToUu5String(e, "progress/get failed"));
+  }
+  if (progress) return progress;
+
+  // try to create progress if it does not exist (get was not successful)
+  try {
+    progress = await this.progressAppClient.post(PROGRESS_USE_CASE_MAP.create, {
+      code: this.code,
+      name: this.name,
+      ...customDtoIn,
+    });
+  } catch (e) {
+    uuConsole.warning(objectToUu5String(e, "progress/create failed"));
+  }
+
+  // raise error if progress does not exist (could not be created)
+  if (progress) {
+    return progress;
+  } else {
+    throw new Errors.ScriptFailed();
+  }
+};
+
+UuProgress.prototype.start = async function (customDtoIn) {
+  try {
+    return await this.progressAppClient.post(PROGRESS_USE_CASE_MAP.start, {
+      code: this.code,
+      lockSecret: this.lockSecret,
+      ...customDtoIn,
+    });
+  } catch (e) {
+    uuConsole.error(objectToUu5String(e, "progress/start failed"));
+    throw new Errors.ScriptFailed();
+  }
+};
+
+UuProgress.prototype.proceed = async function (customDtoIn) {
+  try {
+    return this.progressAppClient.post(PROGRESS_USE_CASE_MAP.proceed, {
+      code: this.code,
+      lockSecret: this.lockSecret,
+      ...customDtoIn,
+    });
+  } catch (e) {
+    uuConsole.warning(objectToUu5String(e, "progress/proceed failed"));
+  }
+};
+
+UuProgress.prototype.end = async function (customDtoIn) {
+  try {
+    return await this.progressAppClient.post(PROGRESS_USE_CASE_MAP.end, {
+      code: this.code,
+      lockSecret: this.lockSecret,
+      ...customDtoIn,
+    });
+  } catch (e) {
+    uuConsole.warning(objectToUu5String(e, "progress/end failed"));
+  }
+};
+
+UuProgress.prototype.releaseLock = async function () {
+  try {
+    return await this.progressAppClient.post(PROGRESS_USE_CASE_MAP.releaseLock, {
+      code: this.code,
+    });
+  } catch (e) {
+    uuConsole.warning(objectToUu5String(e, "progress/releaseLock failed"));
+  }
+};
+
+UuProgress.prototype.setState = async function (customDtoIn) {
+  try {
+    return await this.progressAppClient.post(PROGRESS_USE_CASE_MAP.setState, {
+      code: this.code,
+      ...customDtoIn,
+    });
+  } catch (e) {
+    uuConsole.warning(objectToUu5String(e, "progress/setState failed"));
+  }
+};
+
+UuProgress.prototype.delete = async function () {
+  try {
+    return await this.progressAppClient.post(PROGRESS_USE_CASE_MAP.delete, {
+      code: this.code,
+    });
+  } catch (e) {
+    uuConsole.warning(objectToUu5String(e, "progress/delete failed"));
+  }
+};
+
+UuProgress.prototype.handleError = async function (e) {
+  await this.failed(e);
+  await this.release();
+};
+//@@viewOff:UuProgress
 
 function validateDtoIn(dtoIn) {
   const validator = new Validator(dtoInSchema, true);
@@ -115,10 +252,11 @@ async function callTicketCreate(ticket, sprintId, sprintManClient) {
   };
 
   try {
-    console.info(`HDS3 - Calling ticket/create with dtoIn: ${JSON.stringify(dtoIn)}`);
+    uuConsole.info(objectToUu5String(dtoIn, "HDS6 - Calling ticket/create with dtoIn:"));
     createdTicket = await sprintManClient.post("ticket/create", dtoIn);
   } catch (e) {
-    console.error(`HDS3 - Calling ticket/create with dtoIn: ${JSON.stringify(dtoIn)}`);
+    console.error(`HDS6 - Calling ticket/create with dtoIn: ${JSON.stringify(dtoIn)}`);
+    await uuConsole.error(objectToUu5String(e, `Calling ticket/create failed  for ticket ${dtoIn.name}.`));
     throw new Errors.TicketCreateCall(dtoOut, dtoIn, e);
   }
 
@@ -142,29 +280,104 @@ async function getSession(oidcUri, username, password) {
   return session;
 }
 
-const { dtoIn, console, session } = scriptContext;
+function objectToUu5String(obj, header = "") {
+  let result;
+  let body = "<uu5string.pre>" + JSON.stringify(obj, " ", 2) + "</uu5string.pre>";
+
+  result = `<uu5string/>
+    <UU5.Bricks.Section header="${header}">
+      <UU5.CodeKit.CodeViewer codeStyle="json">${body}</UU5.CodeKit.CodeViewer>
+    </UU5.Bricks.Section>
+  `;
+
+  return result;
+}
+
 const dtoOut = { dtoIn, itemList: [] };
 
 async function main() {
+  await uuConsole.info(objectToUu5String(dtoIn, "Script started with dtoIn"));
+  const hollySession = await getSession(SPRINTMAN_OIDC_URI, HOLLY_CREDENTIALS.username, HOLLY_CREDENTIALS.password);
+
   // HDS 1 - Validation of dtoIn
   dtoOut.uuAppErrorMap = validateDtoIn(dtoIn);
 
-  //HDS 2 - Load data from resource
-  console.info(`HDS2 - Get data from attachmentUri: ${dtoIn.attachmentUri}`);
-  let getScriptResponse = await AppClient.get(dtoIn.attachmentUri, null, { session, enableStreamApi: true });
-  let ticketList = await readReqBody(getScriptResponse.data, true);
+  // HDS 2 - Create uuProgress
+  uuProgress = new UuProgress({
+    code: "ImportSprintmanTicketsProgress",
+    name: "Import Sprintman Tickets Progress",
+    consoleBaseUri: dtoIn.consoleBaseUri,
+    session: hollySession,
+  });
 
-  // HDS 3 - Create tickets in Sprintman
-  const sprintmanSession = await getSession(SPRINTMAN_OIDC_URI, HOLLY_CREDENTIALS.username, HOLLY_CREDENTIALS.password);
-  const sprintManClient = new AppClient({ baseUri: dtoIn.sprintmanBaseUri, session: sprintmanSession });
+  // HDS 3 - Get or create progress
+  let progress = await uuProgress.getOrCreate({
+    authorizationStrategy: "roleGroupInterface",
+    authorizationUriMap: {
+      Authorities: "urn:uu:GGPLUS4U",
+    },
+  });
+  await uuConsole.info("Progress exists and will be used.");
 
-  for (const ticket of ticketList) {
-    let createdTicket = (await callTicketCreate(ticket, dtoIn.sprintId, sprintManClient)).data;
-    dtoOut.itemList.push(createdTicket);
+  // HDS 4 - Start progress
+  await uuProgress.start({
+    doneWork: 0,
+    message: "Script has started. Obtaining tickets from attachment.",
+  });
+
+  //HDS 5 - Load data from resource
+  await uuConsole.info(`HDS5 - Get data from attachmentUri: ${dtoIn.attachmentUri}`);
+
+  let ticketList;
+  try {
+    let getScriptResponse = await AppClient.get(dtoIn.attachmentUri, null, { session, enableStreamApi: true });
+    ticketList = await readReqBody(getScriptResponse.data, true);
+  } catch (e) {
+    await uuConsole.error(objectToUu5String(e, "Get data from attachment failed."));
+    await uuProgress.end({
+      state: "completedWithError",
+      message: "Get data from attachment failed.",
+      doneWork: 0,
+    });
   }
 
+  await uuProgress.proceed({
+    code: "ImportSprintmanTicketsProgress",
+    totalWork: ticketList.length + 1,
+    doneWork: 1,
+    message: "Tickets successfully obtained from attachment.",
+  });
+
+  // HDS 6 - Create tickets in Sprintman
+  const sprintManClient = new AppClient({ baseUri: dtoIn.sprintmanBaseUri, session: hollySession });
+
+  for (let i = 0; i < ticketList.length; i++) {
+    try {
+      let createdTicket = (await callTicketCreate(ticketList[i], dtoIn.sprintId, sprintManClient)).data;
+      dtoOut.itemList.push(createdTicket);
+      await uuProgress.proceed({
+        code: "ImportSprintmanTicketsProgress",
+        totalWork: ticketList.length + 1,
+        doneWork: 2 + i,
+        message: `Ticket ${i + 1} from ${ticketList.length} was successfully imported.`,
+      });
+    } catch (e) {
+      await uuConsole.error(objectToUu5String(e, "Tickets importing failed."));
+      await uuProgress.end({
+        state: "completedWithError",
+        message: "Tickets importing failed.",
+        doneWork: 1 + i,
+      });
+    }
+  }
+
+  uuConsole.info("Script has completed successfully.");
+  await uuProgress.end({
+    state: "completed",
+    message: "Script has completed successfully.",
+  });
+
   //HDS 4
-  console.info("HDS8 - Script finished.");
   return dtoOut;
 }
 
--- uu-sprintman-tool-scriptlib/uu-sprintman-tool-scriptlib-uuscriptlib/test/import-tickets.test.js
+++ uu-sprintman-tool-scriptlib/uu-sprintman-tool-scriptlib-uuscriptlib/test/import-tickets.test.js
@@ -8,6 +8,7 @@ describe("ImportTickets", () => {
       attachmentUri:
         "https://uuapp.plus4u.net/uu-dockit-maing02/7c91c19f9b2c4046a07393c829f64eb6/document/attachment/getData?code=264fd4960fb83cbd2194749d7eaa9bcc&documentId=638733c09de3330036bdf8c2",
       sprintmanBaseUri: "http://localhost:8080/uu-sprintman-maing01/00000503defc438abdf47d1ecd301c67/",
+      consoleBaseUri: "http://localhost:9090/uu-console-maing02/33333333333333333333333333333332/",
       sprintId: "636a2e31156ed20038b8aa6d",
     };
 
